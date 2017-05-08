8c13cd608f396f1e04196748db780307a80c3ca8
adzyuba27 <dzubas@i.ua>
2017-05-08 13:28:54 +0300

    a commit

---
+++ Исходники/Assets/Scripts/BoardManager.cs
@@ -0,0 +1,100 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class BoardManager : MonoBehaviour
+{
+    public static BoardManager instance;
+    public List<Sprite> characters = new List<Sprite>();
+    public GameObject tile;
+    public int xSize, ySize;
+
+    private GameObject[,] tiles;
+
+    void Start()
+    {
+        instance = GetComponent<BoardManager>();
+
+        Vector2 offset = tile.GetComponent<SpriteRenderer>().bounds.size;
+        CreateBoard(offset.x, offset.y);
+        
+    }
+
+    private void CreateBoard(float xOffset, float yOffset)
+    {
+        tiles = new GameObject[xSize, ySize];
+        
+        float startX = this.transform.position.x;
+        float startY = this.transform.position.y;
+
+        Sprite[] previousLeft = new Sprite[ySize];
+        Sprite previousBelow = null; 
+
+        for (int x = 0; x < xSize; x++)
+        {
+            for (int y = 0; y < ySize; y++)
+            {
+                GameObject newTile = (GameObject)Instantiate(tile, new Vector3(startX + (xOffset * x), startY + (yOffset * y), 0), Quaternion.identity);
+                tiles[x, y] = newTile;
+                newTile.transform.parent = transform;
+
+                List<Sprite> possibleCharacters = new List<Sprite>(); 
+                possibleCharacters.AddRange(characters); 
+
+                possibleCharacters.Remove(previousLeft[y]);
+                possibleCharacters.Remove(previousBelow);
+
+                Sprite newSprite = possibleCharacters[Random.Range(0, possibleCharacters.Count)]; 
+
+                newTile.GetComponent<SpriteRenderer>().sprite = newSprite;
+
+                previousLeft[y] = newSprite;
+                previousBelow = newSprite;
+            }
+        }
+    }
+    
+    public IEnumerator FindNullTiles()
+    {
+        for (int x = 0; x < xSize; x++)
+        {
+            for (int y = 0; y < ySize; y++)
+            {
+                if (tiles[x, y].GetComponent<SpriteRenderer>().sprite == null)
+                {
+                    yield return StartCoroutine(ShiftTilesDown(x, y));
+                    break;
+                }
+            }
+        }
+    }
+
+    private IEnumerator ShiftTilesDown(int x, int yStart)
+    {
+        float shiftDelay = 0.03f;
+        List<SpriteRenderer> renders = new List<SpriteRenderer>();
+        int nullCount = 0;
+
+        for (int y = yStart; y < ySize; y++)
+        {  
+            SpriteRenderer render = tiles[x, y].GetComponent<SpriteRenderer>();
+            if (render.sprite == null)
+            { 
+                nullCount++;
+            }
+            renders.Add(render);
+        }
+
+        for (int i = 0; i < nullCount; i++)
+        { 
+            yield return new WaitForSeconds(shiftDelay);
+            for (int k = 0; k < renders.Count-1; k++)
+            { 
+                renders[k].sprite = renders[k + 1].sprite;
+                renders[k + 1].sprite = characters[Random.Range(0, characters.Count)]; 
+            }
+        }
+        
+    }
+
+}
